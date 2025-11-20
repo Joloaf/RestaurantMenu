045c31bc0e6d164e5941352c66ccfb8728a9474d
nightooi <rafaellost2021@outlook.com>
2025-11-20 17:00:10 +0100

    added inline defintion of minimal api
    Signed-off-by: nightooi <rafaellost2021@outlook.com>

--- src/server/RestaurantMenu.API/Program.cs
+++ src/server/RestaurantMenu.API/Program.cs
@@ -1,7 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using RestaurantMenu.Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Scalar.AspNetCore;
+using MinimalApi.Endpoint;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestaurantMenu.Core.Models;
+using Microsoft.AspNetCore.Routing.Matching;
+using Microsoft.AspNetCore.Mvc;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +19,9 @@ builder.Services.AddOpenApi();
 // Configure Database
 builder.Services.AddDbContext<RestaurantDbContex>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IValidateMenu, ValidateMenu>();
+
 
 // Add CORS for development
 builder.Services.AddCors(options =>
@@ -27,6 +36,39 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+app.MapGet("/Menu", async 
+([FromBody] MenuModel model,
+ [FromServices] RestaurantDbContex context,
+ [FromServices] IServiceProvider provider) =>
+{
+    var validator  = provider.GetRequiredService<IValidateMenu>();
+    var dbModelFactory = provider.GetRequiredService<IFactory<Menu>>();
+    Menu modelItem = dbModelFactory.Create();
+    if (validator.ValidateMenu(model))
+    {
+        modelItem.MenuName = model.name;
+        modelItem.UserName = model.userName;
+        modelItem.Id = model.id;
+        modelItem.Theme = model.theme;
+
+        context.Add(modelItem);
+        try
+        {
+            await context.SaveChangesAsync();
+
+        }
+        catch(Exception _)
+        {
+            return Results.InternalServerError();
+        }
+
+        return Results.Accepted();
+    }
+    return Results.BadRequest();
+
+});
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -38,3 +80,39 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.Run();
+
+
+
+public interface IValidateMenu
+{
+    public bool ValidateMenu(MenuModel model);
+}
+
+public class MenuFactory : IFactory<Menu>
+{
+    public Menu Create()
+    {
+        //object creation.
+
+        //Mutate and insert default values here.
+
+        //insert default values here.
+        return new Menu();
+    }
+}
+
+public class ValidateMenu : IValidateMenu
+{
+    bool IValidateMenu.ValidateMenu(MenuModel model)
+    {
+        return true;
+    }
+}
+
+public record MenuModel(int id, string name, string userName, string? theme, int userId);
+
+public interface IFactory<T>
+{
+    T Create();
+}
+
