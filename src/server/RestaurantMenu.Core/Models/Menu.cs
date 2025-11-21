4469d056de430e5a67b67ea5db2932bde635fb1b
Marcus Johnsson <marcus-johnsson@hotmail.se>
2025-11-21 09:58:28 +0100

    removed standard name from kids name

--- src/server/RestaurantMenu.Core/Models/Menu.cs
+++ src/server/RestaurantMenu.Core/Models/Menu.cs
@@ -4,7 +4,7 @@ public class Menu
 {
     public int Id { get; set; }
     public string MenuName { get; set; } // Menu name
-    public string UserName { get; set; } = "Hitta ej"; // Kids name
+    public string UserName { get; set; }  // Kids name
     public string Theme { get; set; }
     public User Users { get; set; }
     public ICollection<Dish> Dishes { get; set; } = new List<Dish>();
