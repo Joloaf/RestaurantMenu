63845880fc18e8f4a38e4a622b71d7c8afc231f4
Marcus Johnsson <marcus-johnsson@hotmail.se>
2025-11-21 09:51:46 +0100

    Update project

---
+++ src/server/RestaurantMenu.API/Service/Validations.cs
@@ -0,0 +1,26 @@
+namespace server.Service;
+
+public class Validations
+{
+    public bool ValidUsername(string username)
+    {
+        return true;
+    }
+    public bool ValidDishName(string username)
+    {
+        return true;
+    }
+    public bool ValidId(int id)
+    {
+        return true;
+    }
+    public bool ValidThemeName(string username)
+    {
+        return true;
+    }
+    public bool TicketValidIdNumber(int id)
+    {
+        return true;
+    }
+    
+}
