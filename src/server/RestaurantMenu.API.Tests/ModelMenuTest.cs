71fbcb00514030f0d4fe78f342785d590d9b7ca2
nightooi <rafaellost2021@outlook.com>
2025-11-21 10:26:59 +0100

    wrote ValidUername_NotNull test
    Signed-off-by: nightooi <rafaellost2021@outlook.com>

--- src/server/RestaurantMenu.API.Tests/ModelMenuTest.cs
+++ src/server/RestaurantMenu.API.Tests/ModelMenuTest.cs
@@ -1,13 +1,34 @@
 using RestaurantMenu.Core.Models;
+using RestaurantMenu.API.Service;
 
 namespace RestaurantMenu.API.Tests;
-
 public class ModelMenuTest
 {
+
+    [Theory]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+    [InlineData("Bartek", true)]
+    [InlineData("           ", false)]
+    public void ValidatUsername_NotNull(string? name, bool expected)
+    {
+        //arrange
+        var sut = new Validations();
+
+        //act
+        var res = sut.ValidUsername(name);
+
+        //assert
+        Assert.Equal(res, expected);
+    }
+
+
     [Fact]
     public void MenuStandardName_ShouldBeNotNull()
     {
         
     }
-    
 }
+
+
+//public static class Use
