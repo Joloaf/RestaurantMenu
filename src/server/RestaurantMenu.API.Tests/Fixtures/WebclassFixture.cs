503f86e5e61739dcbd14c6b9d208f00f75a43179
nightooi <rafaellost2021@outlook.com>
2025-11-24 14:09:38 +0100

    breaking, but need help with resolveing program reference
    Signed-off-by: nightooi <rafaellost2021@outlook.com>

--- src/server/RestaurantMenu.API.Tests/Fixtures/WebclassFixture.cs
+++ src/server/RestaurantMenu.API.Tests/Fixtures/WebclassFixture.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace RestaurantMenu.API.Tests.Fixtures
 {
-    internal class WebclassFixture
+    internal class WebclassFixture<TProgram> : WebApplicationFactory<TProgram>  where TProgram : class
     {
+        public WebclassFixture()
+        {
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                //Step 1: remove real services from our web application to avoid manipulation of real database during testing
+                var descriptor = services.SingleOrDefault(d =>
+                    d.ServiceType == typeof(RestaurantMenu.Infrastructure.Data.RestaurantDbContex));
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                var dbConnectionDescriptor = services.SingleOrDefault(d =>
+                    d.ServiceType == typeof(SqliteConnection));
+                if (dbConnectionDescriptor != null)
+                {
+                    services.Remove(dbConnectionDescriptor);
+                }
+
+                //Step 2: set up services that the test server will use in memory
+                services.AddSingleton<SqliteConnection>(container =>
+                {
+                    var connection = new SqliteConnection("DataSource=:memory:");
+                    connection.Open();
+                    return connection;
+                });
+
+                services.AddDbContext<RestaurantMenu.Infrastructure.Data.RestaurantDbContex>((container, options) =>
+                {
+                    var connection = container.GetRequiredService<SqliteConnection>();
+                    object value = options.UseSqlite(connection);
+                });
+            });
+        }
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            var host = base.CreateHost(builder);
+
+            using var scope = host.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<RestaurantMenu.Infrastructure.Data.RestaurantDbContex>();
+            db.Database.EnsureCreated();
+
+            return host;
+        }
     }
 }
