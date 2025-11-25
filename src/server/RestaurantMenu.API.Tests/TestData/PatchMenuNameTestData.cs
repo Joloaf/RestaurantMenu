5dd0d7fe5b66e84e7ed421668355856786d4c268
nightooi <rafaellost2021@outlook.com>
2025-11-25 17:45:11 +0100

    Patch implemented, tests for http semantics, and object value combination tests should be implemented
    Signed-off-by: nightooi <rafaellost2021@outlook.com>

---
+++ src/server/RestaurantMenu.API.Tests/TestData/PatchMenuNameTestData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+
+namespace RestaurantMenu.API.Tests.TestData;
+
+public class  PatchMenuNameTestData: IEnumerable<object[]>
+{
+    public List<object[]> _testData;
+
+    public PatchMenuNameTestData()
+    {
+        SetModelData();
+    }
+
+    private void SetModelData()
+    {
+        List<object[]> models = [];
+        for (int i = 0; i < 10; i++)
+            models.Add(CreateDataModel());
+        
+        _testData = models;
+    }
+    private object[] CreateDataModel()
+    {
+        var builder = new MenuModelBuilder();
+
+        var obj = builder.WithId(true)
+            .WithIdentityUserId(true)
+            .WithName(true)
+            .WithUserName(true)
+            .WithThemeName(true)
+            .Build();
+
+        var changeObject = builder.WithName(true).Build();
+        return  new[] { obj, (object)changeObject.Menu_mame };
+    }
+    
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        return _testData.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ src/server/RestaurantMenu.API.Tests/TestData/PatchUserNameTestData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace RestaurantMenu.API.Tests.TestData;
+
+public class PatchUserNameTestData : IEnumerable<object[]>
+{
+    public List<object[]> _testData;
+
+    public PatchUserNameTestData()
+    {
+        SetModelData();
+    }
+
+    private void SetModelData()
+    {
+        List<object[]> models = [];
+        for (int i = 0; i < 10; i++)
+            models.Add(CreateDataModel());
+        
+        _testData = models;
+    }
+
+    private object[] CreateDataModel()
+    {
+        var builder = new MenuModelBuilder();
+
+        var obj = builder.WithId(true)
+            .WithIdentityUserId(true)
+            .WithName(true)
+            .WithUserName(true)
+            .WithThemeName(true)
+            .Build();
+
+        var changeObject = builder.WithName(true).Build();
+        
+        return  new[] { obj, (object)obj.User_name };
+    }
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        return _testData.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
