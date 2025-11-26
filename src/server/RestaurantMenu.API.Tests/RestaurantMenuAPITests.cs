d59bd918e00c9470032b5008cd0bfe7df684ebeb
nightooi <rafaellost2021@outlook.com>
2025-11-26 12:30:29 +0100

    After merge into PatchFeature_dev as prep for merge into main, found heisenbug in tests where objects from previous tests were present in db which impacted results of tests, extracted getall test into its own testclass to ensure new creation of the serverfixture
    Signed-off-by: nightooi <rafaellost2021@outlook.com>

--- src/server/RestaurantMenu.API.Tests/RestaurantMenuAPITests.cs
+++ src/server/RestaurantMenu.API.Tests/RestaurantMenuAPITests.cs
@@ -21,6 +21,7 @@ namespace RestaurantMenu.API.Tests
             _client = fixture.CreateClient();
             _fixture = fixture;
         }
+       
          
         [Theory]
         [ClassData(typeof(PatchMenuNameTestData))]
@@ -243,33 +244,5 @@ namespace RestaurantMenu.API.Tests
             Assert.Equal(created.Theme, fetched.Theme);
         }
 
-        [Fact]
-        public async Task ReturnListOfMenu_ValidResponseCode()
-        {
-            var newListMenu = new List<MenuModel>();
-            var userId = await _fixture.AddUsers(_fixture);
-            
-            //Arrange
-            for (int i = 0; i < 3; i++)
-            {
-                var newMenu = new MenuModel(0, $"Read test manu name {i}", $"Read test menu username {i}", $"Read test menu theme {i}",
-                    userId);
-                
-                var createResp = await _client.PostAsJsonAsync(base_url, newMenu);
-                var created = await createResp.Content.ReadFromJsonAsync<MenuModel>();
-                newListMenu.Add(created);
-            }
-
-            //Act
-            var getResp = await _client.GetAsync($"{base_url}all?userId={userId}");
-
-            //Assert
-            getResp.EnsureSuccessStatusCode();
-            var fetched = await getResp.Content.ReadFromJsonAsync<List<MenuModel>>();
-            Assert.NotNull(fetched);
-            Assert.NotEmpty(fetched);
-            Assert.Equal(newListMenu.Count, fetched.Count);
-            
-        }
     }
 }
