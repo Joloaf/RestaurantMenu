e7ddcf400d90e4cbd568a01c80e3808eb05a74e7
Erik Nylund <eriknyl33@hotmail.com>
2025-12-09 11:37:27 +0100

    Orders_And_Tickets

--- src/client/src/lib/components/menuSwitches/orders.svelte
+++ src/client/src/lib/components/menuSwitches/orders.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+    import '../../../app.css';
     import RestMenu from "../RestMenu.svelte";
-    import RestDish from "../RestDish.svelte";
     import type { Menu } from '$lib/services/MenuService';
     import type { Ticket } from "../../../stores/cacheHandlerService"
     import { type Order} from "../../../stores/cacheHandlerService"
@@ -10,18 +10,21 @@
     //bind to tickets
     let i = -1;
     let orders : Order = $state(populateOrder())
+    
 
     function populateOrder() : Order{
-        let i = -1;
         const orders :Order = { } as Order;
         orders.ticket = currentMenu.dishes.map(x => ({ dishes: x, id : i++, quantity: 0 } as Ticket))
         orders.menuId = currentMenu.menuId;
-        orders.ticketNumber = 0;
+        orders.ticketNumber = Math.floor(Math.random() * 100); // Need to change this to a sherd variabel, just random now so it works
 
         return orders;
     }
-    function onClickOrder(){
-
+    function onClickOrder(order: Order) : void {
+        order.ticket = order.ticket.filter(x => x.quantity !== 0)
+        console.log(order)
+        cacheHandlerActions.addOrder(order);
+        orders = populateOrder()
     }
 </script>
 
@@ -35,7 +38,7 @@
         />
         {#each orders.ticket as ticket}
             <div>
-                <img src={ticket.dishes?.dishPicture.length ?? -1 > 0 ? "/pictures/"+ticket.dishes.dishPicture : "/pictures/a70a6112-964d-4f87-8853-0ad44b6d4a3a.png"}/>
+                <img class="order-dish-logo" src={ticket.dishes?.dishPicture.length ?? -1 > 0 ? "/pictures/"+ticket.dishes.dishPicture : "/pictures/a70a6112-964d-4f87-8853-0ad44b6d4a3a.png"}/>
                 <p>{ticket.dishes.dishName}</p>
                 <div>
                     <button onclick={() => ticket.quantity < 100 ? ticket.quantity++ : ticket.quantity }>+</button>
@@ -44,7 +47,7 @@
                 </div>
             </div>
         {/each}
-        <button onclick={onClickOrder}>Beställ</button>
+        <button onclick={() => onClickOrder(orders)}>Beställ</button>
     {:else}
         <p>No menu selected</p>
     {/if}
